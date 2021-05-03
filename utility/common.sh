8e262f751bb48ffd70bbc43f4679f4a21ad899ed
vargalott <vargalott@gmail.com>
2021-05-03 16:14:49 +0300

    fix utility functions

--- utility/common.sh
+++ utility/common.sh
@@ -3,7 +3,7 @@
 source $PROJ_ROOT_DIR/utility/utility.sh
 
 dialog_choose_filepath() {
-  filename=$($DIALOG --title "Choose file" --fselect $HOME/ 100 100 3>&1 1>&2 2>&3)
+  local filename=$($DIALOG --title "Choose file" --fselect $HOME/ 100 100 3>&1 1>&2 2>&3)
 
   case $? in
   $DIALOG_OK)
@@ -22,10 +22,10 @@ dialog_choose_filepath() {
 }
 
 dialog_enter_password() {
-  eval title="$1"
-  eval text="$2"
+  eval local title="$1"
+  eval local text="$2"
 
-  pass=$($DIALOG --title "$title" --insecure --passwordbox "$text" 10 30 3>&1 1>&2 2>&3)
+  local pass=$($DIALOG --clear --title "$title" --insecure --passwordbox "$text" 10 30 3>&1 1>&2 2>&3)
 
   case $? in
   $DIALOG_OK)
@@ -44,20 +44,20 @@ dialog_enter_password() {
 }
 
 dialog_get_sup() {
-  title="ROOT ACCESS IS REQUIRED"
-  text="\nEnter your ROOT password"
+  local title="ROOT ACCESS IS REQUIRED"
+  local text="\nEnter your ROOT password"
   source $PROJ_ROOT_DIR/utility/common.sh dialog_enter_password "\${title}" "\${text}"
-  rpass=$retval
+  local rpass=$retval
 
   export HISTIGNORE='*sudo -S*'
 
   # validate root password(just random command)
-  prompt=$(
+  local prompt=$(
     echo "$rpass" | sudo -S uname -a 2>&1
     sudo -S -nv 2>&1
   )
   if [ $? -ne 0 ]; then
-    $DIALOG --title "Error" --msgbox "Wrong password" 10 40
+    $DIALOG --clear --title "Error" --msgbox "Wrong password" 10 40
     return $RC_ERROR
   fi
 
