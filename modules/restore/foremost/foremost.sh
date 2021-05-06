5dbc10a8ece10ee3e0b9a923d8b6aa741605b5b4
vargalott <vargalott@gmail.com>
2021-05-06 15:48:01 +0300

    implement restore:foremost module

---
+++ modules/restore/foremost/foremost.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+source $PROJ_ROOT_DIR/utility/utility.sh
+
+dialog_modules_restore_foremost_main() {
+  #region ROOT IS REQUIRED
+
+  source $PROJ_ROOT_DIR/utility/common.sh dialog_get_all_partitions
+  if [ $? -eq $RC_ERROR ]; then
+    return
+  fi
+  partitions=$retval
+
+  #endregion
+
+  IFS=', ' read -r -a partitions <<<"$partitions"
+  local menulist=()
+  for i in $(seq 1 ${#partitions[@]}); do
+    menulist+=("$DMENU_OPTION_$i" "${partitions[i - 1]}")
+  done
+
+  while true; do
+    option=$($DIALOG --clear --title "Foremost - Choose partition" \
+      --menu "" 20 50 4 \
+      "${menulist[@]}" \
+      3>&1 1>&2 2>&3)
+
+    case $? in
+    $DIALOG_OK)
+      outdir=$PROJ_ROOT_DIR/out/foremost$(date +%F_%H-%M-%S)
+      mkdir -p "$outdir"
+
+      let len=$(expr ${#menulist[@]} / 2)
+      for i in $(seq 1 $len); do
+        if [ $option -eq $i ]; then
+          let index=$(expr $i + $i)
+          let index=$(expr $index - 1)
+
+          #region ROOT IS REQUIRED
+
+          SUDO_CRED_LOCK_RESET
+
+          source $PROJ_ROOT_DIR/utility/common.sh dialog_get_sup
+          if [ $? -eq $RC_ERROR ]; then
+            continue
+          fi
+          rpass=$retval
+
+          local type=$1
+          eval local title="$3"
+          echo "$rpass" | sudo -S -k foremost -t all -a -i "${menulist[$index]}" -o "$outdir" -v |
+            dialog --clear --title "Foremost - Restoring" --programbox 30 100
+
+          SUDO_CRED_LOCK_RESET
+
+          #endregion
+        fi
+      done
+
+      ;;
+
+    $DIALOG_CANCEL)
+      return $DIALOG_CANCEL
+      ;;
+
+    $DIALOG_ESC)
+      CLEAR_EXIT
+      ;;
+
+    esac
+  done
+}
+
+RESOLVE_FUNC_CALL $@
--- modules/restore/restore.sh
+++ modules/restore/restore.sh
@@ -6,13 +6,18 @@ dialog_module_restore_main() {
   while true; do
     option=$($DIALOG --clear --title "Choose app" \
       --menu "Please select app:" 20 50 4 \
-      "$DMENU_OPTION_1" "Scalpel" 3>&1 1>&2 2>&3)
+      "$DMENU_OPTION_1" "Scalpel" \
+      "$DMENU_OPTION_2" "Foremsot" 3>&1 1>&2 2>&3)
 
     case $? in
     $DIALOG_OK)
       case $option in
       $DMENU_OPTION_1) # show scalpel main menu
         source $PROJ_ROOT_DIR/modules/restore/scalpel/scalpel.sh dialog_modules_restore_scalpel_main
+        ;;
+
+      $DMENU_OPTION_2) # show foremost main menu
+        source $PROJ_ROOT_DIR/modules/restore/foremost/foremost.sh dialog_modules_restore_foremost_main
         ;;
 
       esac
