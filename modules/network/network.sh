f27cfdd3a5a00bb89544c3224f1d6907732181ab
vargalott <vargalott@gmail.com>
2021-05-12 12:12:38 +0300

    add possibility to run test server directly

--- modules/network/network.sh
+++ modules/network/network.sh
@@ -2,13 +2,26 @@
 
 source $PROJ_ROOT_DIR/utility/utility.sh
 
+stop_test_server() {
+  if [ $server_online -eq 1 ]; then
+    curl -X GET localhost:4723/stop
+    server_online=0
+  fi
+}
+
 dialog_module_network_main() {
+  local server_online=0
+
   while true; do
     option=$($DIALOG --clear --title "Choose app" \
       --menu "Please select app:" 20 50 4 \
       "$DMENU_OPTION_1" "Nmap" \
       "$DMENU_OPTION_2" "Siege" \
-      "$DMENU_OPTION_3" "httperf" 3>&1 1>&2 2>&3)
+      "$DMENU_OPTION_3" "httperf" \
+      "" "" \
+      "$DMENU_OPTION_4" "Run test server $([ $server_online -eq 0 ] && echo [offline] || echo [online])" \
+      "$DMENU_OPTION_5" "Stop test server" \
+      3>&1 1>&2 2>&3)
 
     case $? in
     $DIALOG_OK)
@@ -25,15 +38,26 @@ dialog_module_network_main() {
         source $PROJ_ROOT_DIR/modules/network/httperf/httperf.sh dialog_modules_network_httperf_main
         ;;
 
+      $DMENU_OPTION_4) # run test server
+        nohup bash $PROJ_ROOT_DIR/run-ts.sh &>$PROJ_ROOT_DIR/out/nohup_ts$(date +%F_%H-%M-%S) &
+        server_online=1
+        ;;
+
+      $DMENU_OPTION_5) # stop test server
+        stop_test_server
+        ;;
+
       esac
 
       ;;
 
     $DIALOG_CANCEL)
+      stop_test_server
       return $DIALOG_CANCEL
       ;;
 
     $DIALOG_ESC)
+      stop_test_server
       CLEAR_EXIT
       ;;
 
